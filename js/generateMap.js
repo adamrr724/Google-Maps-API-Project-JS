2e334776458956485ced346d02bec45acf2f4f1f
Aundra Miller and Adam Ross Russell <miller.aundra@gmail.com>
2016-03-16 16:12:39 -0700

    adjusted generatemap()

--- js/generateMap.js
+++ js/generateMap.js
@@ -4,24 +4,80 @@ exports.generateMap = function(lat, long) {
   // it 2 arguments: one for latitude, one for longitude.
   // You could refactor this section to pass google maps your own coordinates rather than using geolocation for the user's current location.
   // But you must use coordinates to use this method.
-  console.log(lat);
-  console.log(long);
-  var userLatLng = new google.maps.LatLng(lat, long);
+  // var userLatLng = new google.maps.LatLng(lat, long);
+  //
+  // var myOptions = {
+  //   zoom : 13,
+  //   center : userLatLng,
+  //   mapTypeId : google.maps.MapTypeId.ROADMAP
+  // };
+  // // Draw the map - you have to use 'getElementById' here.
+  // var map = new google.maps.Map(document.getElementById("map"), myOptions);
+  // // Place the marker
+  // new google.maps.Marker({
+  //   map: map,
+  //   position: userLatLng
+  // });
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: lat, lng: long},
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
 
-  var myOptions = {
-    zoom : 10,
-    center : userLatLng,
-    mapTypeId : google.maps.MapTypeId.ROADMAP
-  };
-  // Draw the map - you have to use 'getElementById' here.
-  var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
-  // Place the marker
-  new google.maps.Marker({
-    map: mapObject,
-    position: userLatLng
+
+
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+console.log(searchBox);
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length === 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
   });
 };
-
-function geolocationError(positionError) {
-  alert(positionError);
-}
--- js/locateCity.js
+++ js/locateCity.js
@@ -5,7 +5,6 @@ exports.locateCity = function(address) {
   var latitude = 0;
   var longitude = 0;
   geocoder.geocode( { 'address': address }, function(results, status) {
-    console.log(results);
     if (status == google.maps.GeocoderStatus.OK) {
       latitude = results[0].geometry.location.lat();
       longitude = results[0].geometry.location.lng();
