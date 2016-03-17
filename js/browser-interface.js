99329d147c8970b9b08d74bad229d50212b8d948
Aundra Miller and Adam Ross Russell <adamrr724@yahoo.com>
2016-03-17 08:38:10 -0700

    add search bar to all map searches

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -10,14 +10,20 @@ $(document).ready(function() {
   locateUser();
 
   $('#locateUser').submit(function(event) {
-    console.log("locating user from form")
     event.preventDefault();
+    console.log("locating user from form")
+    $('#map').empty();
+    //input field must be reinstantiated here, or else it the search bar will not show when a new map is created.
+    $('#locateUser').prepend('<input id="pac-input" class="controls" type="text" placeholder="Search Box">');
     locateUser();
   });
 
   $('#myForm').submit(function(event) {
     event.preventDefault();
+    $('#map').empty();
     var address = $('#city_name').val();
+    //input field must be reinstantiated here, or else it the search bar will not show when a new map is created.
+    $('#locateUser').prepend('<input id="pac-input" class="controls" type="text" placeholder="Search Box">');
     locateCity(address);
   });
 
--- js/initAutocomplete.js
+++ js/initAutocomplete.js
@@ -8,7 +8,7 @@ exports.initAutocomplete = function(lat, long) {
   // Create the search box and link it to the UI element.
   var input = document.getElementById('pac-input');
   var searchBox = new google.maps.places.SearchBox(input);
-  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
   // Bias the SearchBox results towards current map's viewport.
   map.addListener('bounds_changed', function() {
@@ -58,6 +58,6 @@ exports.initAutocomplete = function(lat, long) {
       }
     });
     map.fitBounds(bounds);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
   });
 };
